7c32d2d4af0f5b95539bfe8e50c5e3cd11eaae8a
aohuidai <3018234332@tju.edu.cn>
2023-10-13 22:43:50 +0800

    supplement

--- algorithms/federated/main3.sh
+++ algorithms/federated/main3.sh
@@ -14,4 +14,13 @@ elif [ $1 = 3 ]; then
   python3 dt_federated.py --env "hopper-medium-v2" --seed 5 --num_agents 10 --device "cuda:1"
 
   python3 dt_federated.py --env "walker2d-medium-v2" --seed 3 --num_agents 10 --device "cuda:1"
+elif [ $1 = 4 ]; then
+
+  python3 cql_federated.py --env "halfcheetah-medium-v2" --seed 3 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "halfcheetah-medium-v2" --seed 4 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "halfcheetah-medium-v2" --seed 5 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "halfcheetah-medium-v2" --seed 6 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "halfcheetah-medium-v2" --seed 7 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "halfcheetah-medium-v2" --seed 8 --num_agents 10 --device "cuda:1"
 fi
+
