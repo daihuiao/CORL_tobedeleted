e49d2aec2c6576a2015f686c1584353fd5b29230
aohuidai <3018234332@tju.edu.cn>
2023-10-15 14:02:15 +0800

    supplement

--- algorithms/federated/main3.sh
+++ algorithms/federated/main3.sh
@@ -1,19 +1,19 @@
 
 if [ $1 = 1 ];then
-#  python3 cql_federated.py --env "hopper-medium-v2" --seed 3 --num_agents 10 --device "cuda:1"
-#  python3 cql_federated.py --env "hopper-medium-v2" --seed 4 --num_agents 10 --device "cuda:1"
-#  python3 cql_federated.py --env "hopper-medium-v2" --seed 5 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "hopper-medium-v2" --seed 6 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "hopper-medium-v2" --seed 7 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "hopper-medium-v2" --seed 8 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "hopper-medium-v2" --seed 9 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "hopper-medium-v2" --seed 10 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "hopper-medium-v2" --seed 11 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "hopper-medium-v2" --seed 3 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "hopper-medium-v2" --seed 4 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "hopper-medium-v2" --seed 5 --num_agents 10 --device "cuda:1"
 elif [ $1 = 2 ]; then
 
-  python3 cql_federated.py --env "walker2d-medium-v2" --seed 3 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "walker2d-medium-v2" --seed 4 --num_agents 10 --device "cuda:1"
-  python3 cql_federated.py --env "walker2d-medium-v2" --seed 5 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 6 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 7 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 8 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 9 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 10 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 11 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 12--num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 13 --num_agents 10 --device "cuda:1"
+  python3 cql_federated.py --env "walker2d-medium-v2" --seed 14 --num_agents 10 --device "cuda:1"
 elif [ $1 = 3 ]; then
   python3 dt_federated.py --env "hopper-medium-v2" --seed 3 --num_agents 10 --device "cuda:1"
   python3 dt_federated.py --env "hopper-medium-v2" --seed 4 --num_agents 10 --device "cuda:1"
